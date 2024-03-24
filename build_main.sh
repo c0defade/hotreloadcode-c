822d7205b17dcf3dd7764d9f984d4f9c080ff6f8
0xcodefade <0xc0defade@gmail.com>
2024-03-24 11:13:01 -0700

    Loading code dynamically

--- build_main.sh
+++ build_main.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcc -g -o main -I./include -L./lib main.c -lmath
+gcc -g -o main main.c
--- main.c
+++ main.c
@@ -1,8 +1,31 @@
-#include <stdio.h>
-#include "math.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<dlfcn.h>
+
+#define LOGSYM(sym) printf("Loaded %s at: 0x%x\n", #sym, sym);
+
+void *handle = NULL;
+// load symbols into function pointers
+int (*add)(int,int);
+char* (*version)();
+
+void load_symbols() {
+   handle = dlopen("libmath.so", RTLD_LAZY);
+   if (!handle) {
+      printf("Error: %s\n", dlerror());
+      exit(1);
+   }
+   add = dlsym(handle, "add");
+   version = dlsym(handle, "version");
+   LOGSYM(add);
+   LOGSYM(version);
+}
 
 int main() {
-printf("Version: %s\n", version());
-printf("Add 1+2: %d\n", add(1,2));
-return 0;
+   // load symbols from the shared library.
+   load_symbols();
+
+   printf("Version: %s\n", (*version)());
+   printf("Add 1+2: %d\n", (*add)(1,2));
+   return 0;
 }
